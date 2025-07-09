065385c9edb0d054adf385428fcfa596c59128c4
Piotr L <68154041+Sobieskiandy@users.noreply.github.com>
2025-07-09 16:30:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const { Chess } = require('chess.js');
+const path = require('path');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
+let db;
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
-  res.send('Serwer szachowy działa!');
+  res.sendFile(path.join(__dirname, 'animacja.html'));
 });
 
-io.on('connection', (socket) => {
-  const chess = new Chess();
-  console.log('Gracz połączony');
+MongoClient.connect(MONGO_URI)
+  .then(client => {
+    db = client.db('szachy');
+    console.log('Połączono z MongoDB');
 
-  socket.on('ruch', (move) => {
-    if (chess.move(move)) {
-      io.emit('ruch', move);
-    }
+    server.listen(PORT, () => {
+      console.log(`Serwer działa na porcie ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Błąd połączenia z MongoDB:', err);
+  });
+
+io.on('connection', (socket) => {
+  console.log('Nowy gracz połączony');
+
+  socket.on('ruch', async (move) => {
+    console.log('Ruch:', move);
+    await db.collection('ruchy').insertOne(move);
+    io.emit('ruch', move);
   });
 
   socket.on('disconnect', () => {
     console.log('Gracz rozłączony');
   });
-});
-
-server.listen(PORT, () => {
-  console.log(`Działa na porcie ${PORT}`);
 });
