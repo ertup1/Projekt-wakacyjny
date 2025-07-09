c3958f824b2bdd57448bc904dc2f3e494675c231
Piotr L <68154041+Sobieskiandy@users.noreply.github.com>
2025-07-09 16:22:08 +0200

    pliki

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { Chess } = require('chess.js');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Serwer szachowy działa!');
+});
+
+io.on('connection', (socket) => {
+  const chess = new Chess();
+  console.log('Gracz połączony');
+
+  socket.on('ruch', (move) => {
+    if (chess.move(move)) {
+      io.emit('ruch', move);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Gracz rozłączony');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Działa na porcie ${PORT}`);
+});
