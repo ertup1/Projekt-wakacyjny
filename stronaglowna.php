6de1d4fd6d1135025dce38fb718e17adbfb4c0c6
Piotr L <68154041+Sobieskiandy@users.noreply.github.com>
2025-07-11 19:51:32 +0200

    Tymczasowy szkielet do testowania funkcjonalności strony
    W najbliższym czasie będę nieobecny, wobec tego przygotowałem proste strony do testowania różnych funkcjonalności, po moim powrocie zredukuję ilość tych stron do minimum.

---
+++ stronaglowna.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pl-PL">
+<head>
+	<title>Szachy - Strona główna</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<!--Na razie jako osobne strony, kiedy indziej to dopracuję -->
+			<th><img src="logo.png"></th>
+			<th><a href="szukanie.php"><button>Szukaj przeciwnika</button></a></th>
+			<th><a href="wyszukiwanie.php"><button>Szukaj graczy</button></a></th>
+			<th><a href="ranking.php"><button>Ranking</button></a></th>
+			<th><a href="ustawienia.php"><button>Ustawienia</button></a></th>
+			<th><p id="Username">Nazwa gracza</p></th>
+			<th><a href="logowanie.php"><button>Logowanie</button></a></th>
+			<th><a href="rejestracja.php"><button>Rejestracja</button></a></th>
+		</tr>
+	</table>	
+</body>
+</html>
---
+++ szukanie.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pl-PL">
+<head>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+	<script>
+
+	</script>
+	<title>Szachy - Logowanie / Rejestracja</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<th><a href="stronaglowna.php"><img src="logo.png"></a></th>
+			<th><p>Szukaj przeciwnika</p></th>
+			<th><a href="wyszukiwanie.php"><button>Szukaj graczy</button></a></th>
+			<th><a href="ranking.php"><button>Ranking</button></a></th>
+			<th><a href="ustawienia.php"><button>Ustawienia</button></a></th>
+			<th><p id="Username">Nazwa gracza</p></th>
+			<th><a href="logowanie.php"><button>Logowanie</button></a></th>
+			<th><a href="rejestracja.php"><button>Rejestracja</button></a></th>
+		</tr>
+	</table>
+	Szukanie przeciwnika...
+</body>
+</html>
