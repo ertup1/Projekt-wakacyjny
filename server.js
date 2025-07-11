06065623472f1dcba6d4273dc05d1f6d48ff482c
Sobieskiandy <sobieskiandy@gmail.com>
2025-07-11 20:08:08 +0200

    Tymczasowy szkielet do testowania funkcjonalności strony

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   //res.send('Serwer szachowy działa!');
-  res.sendFile(path.join(__dirname, 'stronaglowna.php'));
+  res.sendFile(path.join(__dirname, 'stronaglowna.html'));
 });
 
 io.on('connection', (socket) => {
